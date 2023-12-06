d9bdac99741af5999d4b5423ee1fe2e8ba8ae313
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-12-06 14:09:05 +0100

    Färdig?

---
+++ react-starter-kit/js/movies.js
@@ -0,0 +1,26 @@
+// Vår data (musikalbums)
+var movies = [
+  {
+    title: "The Godfather",
+    year: "1972",
+    genre: "Crime, Drama",
+    rating: "9.2",
+    image: "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
+  },
+  {
+    title: "The Shawshank Redemption",
+    year: "1994",
+    genre: "Crime, Drama",
+    rating: "9.3",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
+  },
+  {
+    title: "The Dark Knight",
+    year: "2008",
+    genre: "Action, Crime, Drama",
+    rating: "9.0",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/1/1c/The_Dark_Knight_%282008_film%29.jpg",
+  },
+];
